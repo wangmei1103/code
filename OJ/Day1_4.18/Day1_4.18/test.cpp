785cff72ab6c15d7d1a6fc3532c36c5cd7766e32
wangmei1103 <2910602199@qq.com>
2021-04-22 21:17:16 +0800

    OJ

--- OJ/Day1_4.18/Day1_4.18/test.cpp
+++ OJ/Day1_4.18/Day1_4.18/test.cpp
@@ -1,32 +1,90 @@
-#include<iostream>
-using namespace std;
-#include<string>
-int main()
-{
-	string str, str2;
-	getline(cin, str);
-	getline(cin, str2);
-	string::iterator it2 = str2.begin();
-	while (it2 != str2.end())
-	{
-		string::iterator it = str.begin();
+//删除公共字符串  https://www.nowcoder.com/practice/f0db4c36573d459cae44ac90b90c6212?tpId=85&tqId=29868&rp=1&ru=%2Factivity%2Foj&qru=%2Fta%2F2017test%2Fquestion-ranking&tab=answerKey
+//
+//#include<iostream>
+//using namespace std;
+//#include<string>
+//int main()
+//{
+//	string str1, str2;
+//	getline(cin, str1);
+//	getline(cin, str2);
+//	int hash[256] = { 0 };
+//	for (int i = 0; i < str2.size(); i++)
+//	{
+//		hash[str2[i]]++;
+//	}
+//	string ret = "";
+//	for (int i = 0; i < str1.size(); i++)
+//	{
+//		if (hash[str1[i]] == 0)
+//		{
+//			ret += str1[i];
+//		}
+//	}
+//	cout << ret << endl;
+//	return 0;
+//}
+//
+////此答案用例只通过75%
+////#include<iostream>
+//using namespace std;
+//#include<string>
+//int main()
+//{
+//	string str, str2;
+//	getline(cin, str);
+//	getline(cin, str2);
+//	string::iterator it2 = str2.begin();
+//	while (it2 != str2.end())
+//	{
+//		string::iterator it = str.begin();
+//
+//		 while (it != str.end())
+//		{
+//			if (*it == *it2)
+//			{
+//				str.erase(it);
+//			}
+//			it++;
+//		}
+//		++it2;
+//	} 
+//	for (auto& e : str)
+//	{
+//		cout << e;
+//	}
+//	return 0;
+//}
 
-		 while (it != str.end())
-		{
-			if (*it == *it2)
-			{
-				str.erase(it);
-			}
-			it++;
-		}
-		++it2;
-	} 
-	for (auto& e : str)
-	{
-		cout << e;
-	}
-	return 0;
-}
+
+//组队竞赛  https://www.nowcoder.com/questionTerminal/6736cc3ffd1444a4a0057dee89be789b?orderByHotValue=1&page=1&onlyReference=false
+//#include<iostream>
+//using namespace std;
+//#include<algorithm>
+//#include<vector>
+//int main()
+//{
+//	int n;
+//	//cin >> n;
+//	//循环输入
+//	while (cin >> n)
+//	{
+//		long long sum = 0;
+//		vector<int> a;
+//		a.resize(3 * n);
+//		for (int i = 0; i < 3 * n; i++)
+//		{
+//			cin >> a[i];
+//		}
+//		sort(a.begin(), a.end());
+//		for (int i = 0; i < n; i++)
+//		{
+//			sum = sum + a[a.size() - 2 * (i + 1)];
+//		}
+//		cout << sum << endl;
+//	}
+//	return 0;
+//}
 
 //#include<iostream>
 //using namespace std;
@@ -55,12 +113,13 @@ int main()
 //	}
 //	cout << count << endl;
 //}
-////int main()
-////{
-////	test();
-////	return 0;
-////}
-//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+
 //int main()
 //{
 //	string str("they are student haha");
--- OJ/Day4_4.21/Day4_4.21/test.cpp
+++ OJ/Day4_4.21/Day4_4.21/test.cpp
@@ -1,49 +1,135 @@
 #include<iostream>
 using namespace std;
-
-class A {
-public:
-	A(const char *s)
-	{
-		cout << s << endl;
-	}
-	~A() {}
-};
-class B:virtual public A
-{
-public:
-	B(const char *s1, const char *s2)
-		:A(s1)
-	{
-		cout << s2 << endl;
-	}
-};
-class C :virtual public A
-{
-public:
-	C(const char *s1,const char *s2)
-		:A(s1)
-	{
-		cout << s2 << endl;
-	}
-};
-class D :public B, public C
-{
-public:
-	D(const char *s1, const char *s2, const char *s3, const char *s4)
-		:B(s1, s2)
-		, C(s1, s3)
-		, A(s1)
-	{
-		cout << s4 << endl;
-	}
-};
+#include<stack>
 int main()
 {
-	D *p = new D("class A", "class B", "class C", "class D");
-	delete p;
+	int M;
+	int N;
+	cin >> M >> N;
+	stack<int> st;
+	int ret;
+	while (M != 0)
+	{
+		if (M < 0)
+		{
+			M = -M;
+			cout << "-";
+		}
+		ret = M % N;
+		st.push(ret);
+		M = M / N;
+	}
+	while (!st.empty())
+	{
+		if (st.top() == 10)
+		{
+			cout << 'A';
+			st.pop();
+			continue;
+		}
+		if (st.top() == 11)
+		{
+			cout << 'B';
+			st.pop();
+			continue;
+		}
+		if (st.top() == 12)
+		{
+			cout << 'C';
+			st.pop();
+			continue;
+		}
+		if (st.top() == 13)
+		{
+			cout << 'D';
+			st.pop();
+			continue;
+		}
+		if (st.top() == 14)
+		{
+			cout << 'E';
+			st.pop();
+			continue;
+		}
+		if (st.top() == 15)
+		{
+			cout << 'F';
+			st.pop();
+			continue;
+		}
+		if (st.top() < 10)
+		
+		{
+			cout << st.top();
+			st.pop();
+			continue;
+		}
+	}
+	cout << endl;
 	return 0;
 }
+
+//int main()
+//{
+//	int a, b, c, d;
+//	cin >> a >> b >> c >> d;
+//	int A = (a + c) / 2;
+//	int B = (c - a) / 2;
+//	int C = (d - b) / 2;
+//	int B2 = (b + d) / 2;
+//	if (B == B2)
+//	{
+//		cout << A << " " << B << " " << C;
+//	}
+//	else
+//		cout << "No" << endl;
+//	return 0;
+//}
+
+//
+//class A {
+//public:
+//	A(const char *s)
+//	{
+//		cout << s << endl;
+//	}
+//	~A() {}
+//};
+//class B:virtual public A
+//{
+//public:
+//	B(const char *s1, const char *s2)
+//		:A(s1)
+//	{
+//		cout << s2 << endl;
+//	}
+//};
+//class C :virtual public A
+//{
+//public:
+//	C(const char *s1,const char *s2)
+//		:A(s1)
+//	{
+//		cout << s2 << endl;
+//	}
+//};
+//class D :public B, public C
+//{
+//public:
+//	D(const char *s1, const char *s2, const char *s3, const char *s4)
+//		:B(s1, s2)
+//		, C(s1, s3)
+//		, A(s1)
+//	{
+//		cout << s4 << endl;
+//	}
+//};
+//int main()
+//{
+//	D *p = new D("class A", "class B", "class C", "class D");
+//	delete p;
+//	return 0;
+//}
 
 //int main()
 //{
